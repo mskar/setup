fbdc3fa78bc97cf059f7b7176868fb324bb458ad
Martin Skarzynski <marskar@gmail.com>
2020-01-02 11:35:35 -0500

    add scripts to update dotfiles

---
+++ cp_dotfiles.sh
@@ -0,0 +1,12 @@
+# karabiner
+cp ~/py4ds/setup/.zshrc ~
+# ideavim
+cp ~/py4ds/setup/.ideavimrc ~
+# iterm
+cp ~/py4ds/setup/com.googlecode.iterm2.plist ~
+# vim
+cp ~/py4ds/setup/vim/.vimrc* ~
+# neovim
+cp ~/py4ds/setup/neovim/*.vim ~/.config/nvim/
+# karabiner
+cp ~/py4ds/setup/karabiner.json ~/.config/karabiner/
