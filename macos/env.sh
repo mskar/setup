cc3b3dece46c4f66f83f86671d57516757ce1ba5
Martin Skarzynski <marskar@gmail.com>
2020-02-13 12:54:19 -0500

    add jupyterlab-vim installation to macos/env.sh

--- macos/env.sh
+++ macos/env.sh
@@ -1,3 +1,9 @@
 conda create -yc conda-forge -n py python=3.8 joblib jupyterlab matplotlib numpy pandas scikit-learn scipy
 
 conda create -yc conda-forge -n r rstudio r-essentials r-tidymodels r-tidyverse
+
+conda activate py
+
+jupyter labextension install jupyterlab_vim
+
+conda deactivate
