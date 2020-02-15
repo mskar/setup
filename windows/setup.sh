769002eb843f4a1f5e236561fafa72a4af234cf3
Martin Skarzynski <marskar@gmail.com>
2020-02-15 13:25:51 -0500

    need to sudo apt install zsh before oh my zsh on windows

--- windows/setup.sh
+++ windows/setup.sh
@@ -10,6 +10,18 @@
 ## Enable clipboard
 ## Clean up taskbar
 
+# Shell programs needed for aliases
+## Install fzf (fuzzy finder)
+## Install bat and exa (for fzf file preview)
+## Install fasd and fd (to provide inputs for fzf)
+## Install r (so that the rmarkdown render alias and Nvim-R work in base environment)
+## Install xpdf (e.g. pdftotext - for fzf PDF file preview)
+## Install vim and neovim
+sudo apt install bat exa fasd fd fzf hub neovim r sc-im tmux vim xpdf zsh
+
+## Install fzf key bindings and fuzzy completion
+$(brew --prefix)/opt/fzf/install --no-bash --no-fish --key-bindings --completion --update-rc
+
 ## Install oh-my-zsh et al.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ### When prompted to change shell, enter Y
@@ -35,16 +47,6 @@ code --install-extension ms-vsliveshare.vsliveshare
 
 code --install-extension vscodevim.vim
 
-# Shell programs needed for aliases
-## Install fzf (fuzzy finder)
-## Install bat and exa (for fzf file preview)
-## Install fasd and fd (to provide inputs for fzf)
-## Install r (so that the rmarkdown render alias and Nvim-R work in base environment)
-## Install xpdf (e.g. pdftotext - for fzf PDF file preview)
-## Install vim and neovim
-apt install bat exa fasd fd fzf hub neovim r sc-im tmux vim xpdf
-## Install fzf key bindings and fuzzy completion
-$(brew --prefix)/opt/fzf/install --no-bash --no-fish --key-bindings --completion --update-rc
 ## Download dotfiles (configuration files)
 ### Vim
 curl https://raw.githubusercontent.com/py4ds/setup/master/vim/.vimrc.local -o ~/.vimrc.local
