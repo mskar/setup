20053d8bfcddb5b49170a40da99ff40db28e9260
Martin Skarzynski <marskar@gmail.com>
2020-02-26 12:44:00 -0500

    add windows specific cp_dotfiles script

---
+++ windows/cp_dotfiles.sh
@@ -0,0 +1,3 @@
+cp ~/py4ds/setup/windows/profiles.json /mnt/c/Users/*/AppData/Local/Packages/Microsoft.WindowsTerminal_*/LocalState/
+cp ~/py4ds/setup/windows/.*rc ~
+cp ~/py4ds/setup/windows/.gitconfig ~
