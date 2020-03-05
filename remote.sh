27c0b1e2c10f7b66774d93c778a4bbe9e9654d32
Martin Skarzynski <marskar@gmail.com>
2020-03-05 15:44:32 -0500

    udpate remote.sh

--- remote.sh
+++ remote.sh
@@ -1,7 +1,7 @@
 git remote add bit https://mskar@bitbucket.org/mskar/setup.git
-git remote add hub https://github.com/py4ds/setup
-git remote add lab https://gitlab.com/py4ds/setup
+git remote add hub https://github.com/mskar/setup
+git remote add lab https://gitlab.com/mskar/setup
 git remote remove origin
-git remote add origin https://github.com/py4ds/setup
-git remote set-url origin https://gitlab.com/py4ds/setup --add
+git remote add origin https://github.com/mskar/setup
+git remote set-url origin https://gitlab.com/mskar/setup --add
 git remote set-url origin https://mskar@bitbucket.org/mskar/setup.git --add
