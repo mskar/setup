cb665e3e60447fb011094b921d69c780d8af2284
Martin Skarzynski <marskar@gmail.com>
2020-10-24 00:20:00 -0400

    create script for installing vs code extensions

---
+++ scripts/code.sh
@@ -0,0 +1,4 @@
+code --install-extension ms-python.python --force
+code --install-extension vscodevim.vim --force
+code --install-extension ms-vsliveshare.vsliveshare --force
+code --install-extension ms-python.vscode-pylance --force
