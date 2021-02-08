d4310ea6bca4df6cfce1b6fef273b26a9beea061
Martin Skarzynski <marskar@gmail.com>
2021-02-08 13:12:55 -0500

    use hammerspoon to avoid accidentally quitting applications with cmd q

---
+++ init.lua
@@ -0,0 +1,18 @@
+-- https://github.com/pqrs-org/Karabiner-Elements/issues/242#issuecomment-277330358
+-- Press Cmd+Q twice to quit
+
+local quitModal = hs.hotkey.modal.new('cmd','q')
+
+function quitModal:entered()
+    hs.alert.show("Press Cmd+Q again to quit", 1)
+    hs.timer.doAfter(1, function() quitModal:exit() end)
+end
+
+local function doQuit()
+    local app = hs.application.frontmostApplication()
+    app:kill()
+end
+
+quitModal:bind('cmd', 'q', doQuit)
+
+quitModal:bind('', 'escape', function() quitModal:exit() end)
