61ec923d312fc4e75a0c4ccaeb1714d03dd1cf21
Martin Skarzynski <marskar@gmail.com>
2021-10-01 14:19:52 -0400

    working on browser shortcuts

--- surfingkeys.js
+++ surfingkeys.js
@@ -5,7 +5,7 @@ aceVimMap('<C-u>', '<Esc>d0xi', 'insert');
 Hints.style('font-size: 13pt;')
 
 
-imapkey('<Ctrl-w>', '#15Delete a word backwards', function() {
+imapkey('<Ctrl-w>', 'Delete a word backwards', function() {
     var element = getRealEdit();
     if (element.setSelectionRange !== undefined) {
         var pos = deleteNextWord(element.value, -1, element.selectionStart);
@@ -21,3 +21,23 @@ imapkey('<Ctrl-w>', '#15Delete a word backwards', function() {
         selection.setPosition(selection.focusNode, p1);
     }
 });
+
+
+
+imapkey('<Alt-d>', 'Delete a word forwards', function() {
+            var element = getRealEdit();
+        if (element.setSelectionRange !== undefined) {
+            var pos = deleteNextWord(element.value, 1, element.selectionStart);
+            element.value = pos[0];
+            element.setSelectionRange(pos[1], pos[1]);
+        } else {
+            // for contenteditable div
+            var selection = document.getSelection();
+            var p0 = selection.focusOffset;
+            document.getSelection().modify("move", "forward", "word");
+            var v = selection.focusNode.data, p1 = selection.focusOffset;
+            selection.focusNode.data = v.substr(0, p0) + v.substr(p1);
+            selection.setPosition(selection.focusNode, p0);
+        }
+    }
+);
